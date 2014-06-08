e03f3773c6075ce8d994127f97e7c8baaf5b8624
cyclamenkde@gmail.com <cyclamenkde>
2014-06-08 21:37:28 +0800

    首次提交到本地仓库

---
+++ WXForJava/src/org/eagtop/wx/message/req/TextMessage.java
@@ -0,0 +1,14 @@
+package org.eagtop.wx.message.req;
+
+public class TextMessage extends BaseMessage{
+	// ÏûÏ¢ÄÚÈİ  
+	private String Content;  
+
+	public String getContent() {  
+	    return Content;  
+	}  
+
+	public void setContent(String content) {  
+	    Content = content;  
+	}  
+}
---
+++ WXForJava/src/org/eagtop/wx/user/Manager.java
@@ -0,0 +1,22 @@
+package org.eagtop.wx.user;
+
+public class Manager {
+	
+	public boolean Add(String username) 
+	{
+		//TODO 用户添加
+		return false;
+	}
+
+	public boolean Update(String username) 
+	{
+		//TODO 用户修改
+		return false;
+	}
+	
+	public boolean Get(String username) 
+	{
+		//TODO 用户查询
+		return false;
+	}
+}
